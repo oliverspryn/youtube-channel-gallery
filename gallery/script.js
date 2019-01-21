6decf23f51038dd900c8d1021005a27da6080486
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-01-21 16:02:44 -0500

    No idea

--- gallery/script.js
+++ gallery/script.js
@@ -6,5 +6,14 @@ Vue.component('youtube-gallery', {
 })
 
 const gallery = new Vue({
-    el: '#gallery'
+    el: '#gallery',
+    created: () => {
+        axios.get('https://content.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=1&playlistId=UUuCkxoKLYO_EQ2GeFtbM_bw&order=date&key=AIzaSyC5hcZ0ZMY0fZDdyEzU1mSSyYAegYaYZ0c')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 })
